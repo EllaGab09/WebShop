2511987c5c953a3d98c2e5c3c5cfa78565385898
Jimmy <Jimmyhaglund@gmail.com>
2021-07-16 13:53:58 +0200

    Add IamApp

---
+++ IAMApp_03/IAM/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IAM.Areas.Identity.Data;
+using IAM.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IAM.Areas.Identity.IdentityHostingStartup))]
+namespace IAM.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IAMContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IAMContextConnection")));
+
+                services.AddDefaultIdentity<IAMUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IAMContext>();
+            });
+        }
+    }
+}
---
+++ IAMApp_03/IAM/Models/LoginCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace IAM.Models
+{
+    public class LoginCredentials
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
