4b1425ea3d4fa932b44fe41e8d1407176860ba6d
jimmy.nordin.1979@gmail.com <jimmy.nordin.1979@gmail.com>
2021-08-03 16:31:24 +0200

    Reversing the merge

---
+++ WebShopApp/WebShop/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShopDB.Models
+{
+    public class Category
+    {
+        //[Key]
+        public int ID { get; set; }
+        //[Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ WebShopApp/WebShop/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShopDB.Models
+{
+    
+    public class User
+    {
+        //[Key]
+        //public int ID { get; set; }
+        //[Required]
+        //public string Name { get; set; }
+        //[Required]
+        //public string Email { get; set; }
+        //[Required]
+        //public string Password { get; set; }
+        //[Required]
+        //public string Phone { get; set; }
+        //[Required]
+        //public string BillingAddress { get; set; }
+        //[Required]
+        //public string ShippingAddress { get; set; }
+        //public ICollection<Order> Orders { get; set; }
+    }
+}
